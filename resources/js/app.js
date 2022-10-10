01d414e68a5c98ae2d524186deae83f8da432fbb
nelsonBBbatak81 <butarpondoh@gmail.com>
2022-10-10 17:56:32 +0700

    Install laravel-mix including others dependency to build application using vue and laravel

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,20 @@
-import './bootstrap';
+import "./bootstrap";
+import router from "./router/index";
+// import { registerBaseComponents } from "./helpers/registerBaseComponents";
+import Layout from "./layout/Layout.vue";
+import Navbar from "./components/Header/Navbar.vue";
+import FooterContent from "./components/Footer/Footer.vue";
+
+import { createApp } from "vue";
+
+const app = createApp({});
+
+// registerBaseComponents(app);
+
+app.component("nav-bar", Navbar);
+app.component("footer-content", FooterContent);
+app.component("layout-component", Layout);
+
+app.use(router);
+
+app.mount("#app");
---
+++ resources/js/router/routes.js
@@ -0,0 +1,10 @@
+// Pages
+import Index from "../pages/Index.vue";
+import About from "../pages/About.vue";
+
+const routes = [
+    { path: "/", component: Index },
+    { path: "/about", component: About },
+];
+
+export default routes;
